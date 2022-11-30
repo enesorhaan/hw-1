6de249acd9cf901bf84d7952d810136874c67725
Enes Orhan <enesorhan_1366@hotmail.com>
2022-11-30 14:45:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double boy, kilo, indeks;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+        boy = inp.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kilo = inp.nextDouble();
+
+        indeks = (kilo/(boy*boy));
+
+        System.out.print("Vücut Kitle İndeksiniz: " + indeks);
+    }
+}
